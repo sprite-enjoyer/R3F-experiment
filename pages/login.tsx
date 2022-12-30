0dfe101a55a10e0aa82232e77339b1fef8e1520b
sprite-enjoyer <samkharadze.saba01@gmail.com>
2022-12-30 08:07:19 +0400

    generic button component, more oAuth providers

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,33 +1,36 @@
 import styles from "../styles/login.module.scss";
-import { Container } from "@nextui-org/react";
-import GoogleButton from "../components/GoogleButton";
+import { Button, Container } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
+import SignInWithProviderButton from "../components/SignInWithProviderButton";
 
 const Login = () => {
   const router = useRouter();
   const { data } = useSession();
 
   if (data) {
-    router.replace(`user/${data.user?.email}/profile?sessionExists=true&localSession=false`);
+    router.replace(`user/${data.user?.email}/profile`);
 
     return <div className={styles["loading"]} >
       <h1>Loading...</h1>
     </div>
   }
 
-  else {
-
-    return (
-      <div className={styles["root"]} >
-        <Container className={styles["main"]} >
-          <Container className={styles["main__container"]} >
-            <GoogleButton />
-          </Container>
+  return (
+    <div className={styles["root"]} >
+      <Container className={styles["main"]} >
+        <Container className={styles["main__container"]} >
+          <span className={styles["main__container__txt"]} >Sign In with:</span>
+          <div className={styles["main__container__button-group"]} >
+            <SignInWithProviderButton providerName="google" onClickHandler={async () => await signIn("google")} />
+            <SignInWithProviderButton providerName="facebook" onClickHandler={async () => await signIn("facebook")} />
+            <SignInWithProviderButton providerName="spotify" onClickHandler={async () => await signIn("spotify")} />
+          </div>
         </Container>
-      </div>
-    );
-  }
+      </Container>
+    </div>
+  );
 }
 
 export default Login;
